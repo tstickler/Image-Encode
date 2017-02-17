00a50b37b0fa2afb277155d5da948adf9f2ab224
tstickler <tstickler17@yahoo.com>
2017-02-17 00:22:30 -0800

    renamed and reorganized variables

--- encypt.py
+++ encypt.py
@@ -6,117 +6,123 @@ import math
 # Library for argument parsing
 import argparse
 
-# User defined message to encrypt
-message = "Tyler Stickler"
-
-# Determines message length, zero fills it up to 33
-msgLength = len(message)
-binaryMsgLength = bin(msgLength)[2:].zfill(33)
-pixelsNeeded = math.ceil((msgLength * 8)/3)
-print("Pixels Needed:", pixelsNeeded)
-print("Message length:", msgLength)
-print("Binary Message length:", binaryMsgLength)
-
-# Creates an array to hold binary values of the letters
-messageArray = []
-
-# Changes each letter to binary, then adds it to the message array
-for letter in message:
-    binaryVal = bin(ord(letter))[2:].zfill(8)
-    messageArray.append(binaryVal)
-binaryMessage = "".join(messageArray)
-print("Message:", binaryMessage)
-
-# Sets the file variable to the image the user has entered
-file = "zelda.jpg"
-
-# Splits the file name from the extension so we can change it to png
-fileName, fileExt = os.path.splitext(file)
-
-# Creates an image object to allow us to interact with the user's image
-im = Image.open(file)
-
-# Gets the width and height of our image
-width, height = im.size
-
 
 #######################################################################
-# This function inputs the data into the image. It grabs a pixel at a #
-# certain xy coordinate, starting at the bottom left, converts its    #
+# This function inputs data into an image. It grabs a pixel at a      #
+# certain xy coordinate, starting at the bottom right, converts its   #
 # RGB value into binary, then modifies the least significant bit to   #
-# hide our data.                                                      #
+# hide our data. This continues until the whole message is encoded.   #
+#                                                                     #
+# Function input: Image object                                        #
+# Function returns: None                                              #
 #######################################################################
-def modifyPixel():
+def encode_message(im):
+    # User defined message to encrypt
+    message = "Tyler Stickler"
+
+    # Determines message length, zero fills it up to 33
+    msgLength = len(message)
+    binaryMsgLength = bin(msgLength)[2:].zfill(33)
+    pixels_needed = math.ceil((msgLength * 8) / 3)
+    print("Pixels Needed:", pixels_needed)
+    print("Message length:", msgLength)
+    print("Binary Message length:", binaryMsgLength)
+
+    # Creates an array to hold binary values of the letters
+    messageArray = []
+
+    # Changes each letter to binary, then adds it to the message array
+    for letter in message:
+        binaryVal = bin(ord(letter))[2:].zfill(8)
+        messageArray.append(binaryVal)
+    binaryMessage = "".join(messageArray)
+    print("Message:", binaryMessage)
+
+    # Determines the width and height of the image
+    width, height = im.size
+
     # The width and height modifiers are used to move us between pixels
-    widthMod = 1
-    heightMod = 1
+    width_mod = 1
+    height_mod = 1
 
     # Determines how many times we need to loop
-    timesToLoop = pixelsNeeded + 11
+    times_to_loop = pixels_needed + 11
 
     # Used as indexes
-    lengthCounter = 32
-    letterCounter = 0
+    length_counter = 32
+    letter_counter = 0
 
     # Loop through and modify the pixels until task is complete
-    while timesToLoop > 0:
+    while times_to_loop > 0:
         # Gets the pixel at the current xy coordinate
-        r, g, b = im.getpixel((width-widthMod, height-heightMod))
+        r, g, b = im.getpixel((width-width_mod, height-height_mod))
 
         # Converts the RGB values to binary
-        binaryRed = list(bin(r)[2:].zfill(8))
-        binaryGreen = list(bin(g)[2:].zfill(8))
-        binaryBlue = list(bin(b)[2:].zfill(8))
+        binary_red = list(bin(r)[2:].zfill(8))
+        binary_green = list(bin(g)[2:].zfill(8))
+        binary_blue = list(bin(b)[2:].zfill(8))
 
         # Fills the pixel with a new RGB value
-        if lengthCounter > 0:
+        if length_counter > 0:
             # Take the least significant bit and modify it according
-            binaryRed[7] = binaryMsgLength[lengthCounter]
-            lengthCounter -= 1
-            binaryRed = int("".join(binaryRed), 2)
+            binary_red[7] = binaryMsgLength[length_counter]
+            length_counter -= 1
+            binary_red = int("".join(binary_red), 2)
 
-            binaryGreen[7] = binaryMsgLength[lengthCounter]
-            lengthCounter -= 1
-            binaryGreen = int("".join(binaryGreen), 2)
+            binary_green[7] = binaryMsgLength[length_counter]
+            length_counter -= 1
+            binary_green = int("".join(binary_green), 2)
 
-            binaryBlue[7] = binaryMsgLength[lengthCounter]
-            lengthCounter -= 1
-            binaryBlue = int("".join(binaryBlue), 2)
+            binary_blue[7] = binaryMsgLength[length_counter]
+            length_counter -= 1
+            binary_blue = int("".join(binary_blue), 2)
 
-            im.putpixel((width - widthMod, height - heightMod),
-                        (binaryRed, binaryGreen, binaryBlue))
-        elif timesToLoop > 0:
+            im.putpixel((width - width_mod, height - height_mod),
+                        (binary_red, binary_green, binary_blue))
+        elif times_to_loop > 0:
             # Modify the RGB to insert our message
-            if letterCounter < msgLength * 8:
-                binaryRed[7] = binaryMessage[letterCounter]
-                letterCounter += 1
+            if letter_counter < msgLength * 8:
+                binary_red[7] = binaryMessage[letter_counter]
+                letter_counter += 1
 
-            if letterCounter < msgLength * 8:
-                binaryGreen[7] = binaryMessage[letterCounter]
-                letterCounter += 1
+            if letter_counter < msgLength * 8:
+                binary_green[7] = binaryMessage[letter_counter]
+                letter_counter += 1
 
-            if letterCounter < msgLength * 8:
-                binaryBlue[7] = binaryMessage[letterCounter]
-                letterCounter += 1
+            if letter_counter < msgLength * 8:
+                binary_blue[7] = binaryMessage[letter_counter]
+                letter_counter += 1
 
-            binaryRed = int("".join(binaryRed), 2)
-            binaryGreen = int("".join(binaryGreen), 2)
-            binaryBlue = int("".join(binaryBlue), 2)
+            binary_red = int("".join(binary_red), 2)
+            binary_green = int("".join(binary_green), 2)
+            binary_blue = int("".join(binary_blue), 2)
 
-            im.putpixel((width - widthMod, height - heightMod),
-                        (binaryRed, binaryGreen, binaryBlue))
+            im.putpixel((width - width_mod, height - height_mod),
+                        (binary_red, binary_green, binary_blue))
 
         # Move us to the next pixel in the row
-        widthMod += 1
-        timesToLoop -= 1
+        width_mod += 1
+        times_to_loop -= 1
 
         # If we hit the left side of the picture, move up a row
-        if widthMod-1 == width:
-            heightMod += 1
-            widthMod = 1
+        if width_mod-1 == width:
+            height_mod += 1
+            width_mod = 1
 
 
-def decryptMessage():
+#######################################################################
+# This function extracts data from an image. It grabs a pixel at a    #
+# certain xy coordinate, starting at the bottom right, determines the #
+# length and content of the message by checking the least significant #
+# bit of each RGB value and translates it into a readable string.     #
+#                                                                     #
+# Function input: Image object                                        #
+# Function returns: Decoded message                                   #
+#######################################################################
+def decode_message(im):
+    # Determines the width and height of the image
+    width, height = im.size
+
     # The width and height modifiers are used to move us between pixels
     width_mod = 1
     height_mod = 1
@@ -200,16 +206,24 @@ def decryptMessage():
         i += 8
 
     decoded_message = "".join(decoded_message)
-
-# saves our image in png format
-# im.save("{}.png".format(fileName))
+    return decoded_message
 
 
 def main():
+    # Sets the file variable to the image the user has entered
+    file = "zelda.jpg"
 
-    modifyPixel()
-    # im.show()
-    decryptMessage()
+    # Splits the file name from the extension so we can change it to png
+    file_name, file_extension = os.path.splitext(file)
+
+    # Creates an image object to allow us to interact with the user's image
+    im = Image.open(file)
+
+    encode_message(im)
+    print(decode_message(im))
+
+    # saves our image in png format
+    # im.save("{}.png".format(file_name))
 
 if __name__ == "__main__":
     main()
