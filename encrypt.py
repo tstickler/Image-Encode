2c0176fe39400d900d297f661094c42ed1f423fd
tstickler <tstickler17@yahoo.com>
2017-02-18 13:56:02 -0800

    modified comments, removed unneeded variables

--- encrypt.py
+++ encrypt.py
@@ -1,20 +1,28 @@
+###############################################################################
+# Programmer: Tyler Stickler                                                  #
+# File name: encrypt.py                                                       #
+# Description: This program takes can do two things. It can encode a message  #
+#              into a picture or it can decode a message that is already      #
+#              encoded. The user can decide which option to use by specifying #
+#              their arguments in the command line.                           #
+###############################################################################
+
 from PIL import Image
-import os
 import math
 import sys
 import argparse
 
 
-#######################################################################
-# This function inputs data into an image. It grabs a pixel at a      #
-# certain xy coordinate, starting at the bottom right, converts its   #
-# RGB value into binary, then modifies the least significant bit to   #
-# hide our data. This continues until the whole message is encoded.   #
-#                                                                     #
-# Function input: Image object, file name, file extension             #
-# Function returns: None                                              #
-#######################################################################
-def encode_message(im, file_name, file_extension, user_message, output_file):
+###############################################################################
+# This function inputs data into an image. It grabs a pixel at a certain xy   #
+# coordinate, starting at the bottom right, converts its RGB value into       #
+# binary, then modifies the least significant bit to hide our data. This      #
+# continues until the whole message is encoded.                               #
+#                                                                             #
+# Function input: Image object, message to encode, output file                #
+# Function returns: None                                                      #
+###############################################################################
+def encode_message(im, user_message, output_file):
     # User defined message to encrypt
     message = user_message
 
@@ -23,18 +31,20 @@ def encode_message(im, file_name, file_extension, user_message, output_file):
     pixels_in_image = width * height
 
     # Determines message length, zero fills it up to 33
-    msgLength = len(message)
-    binaryMsgLength = bin(msgLength)[2:].zfill(33)
-    pixels_needed = math.ceil((msgLength * 8) / 3)
+    message_length = len(message)
+    binaryMsgLength = bin(message_length)[2:].zfill(33)
+    pixels_needed = math.ceil((message_length * 8) / 3)
 
+    # Determines if their are enough pixels in the image to add the message
     if pixels_needed + 11 > pixels_in_image:
-        print("Sorry, your image is too small to hold this big of a message")
+        print("Sorry, your image is too small to hold this big of a message.")
         sys.exit()
 
     # Creates an array to hold binary values of the letters
     messageArray = []
 
-    # Changes each letter to binary, then adds it to the message array
+    # Changes each letter to binary, adds it to the message array, and converts
+    # it into a string
     for letter in message:
         binaryVal = bin(ord(letter))[2:].zfill(8)
         messageArray.append(binaryVal)
@@ -53,17 +63,17 @@ def encode_message(im, file_name, file_extension, user_message, output_file):
 
     # Loop through and modify the pixels until task is complete
     while times_to_loop > 0:
-        # Gets the pixel at the current xy coordinate
+        # Gets the pixel's RGB values at the current XY coordinate
         r, g, b = im.getpixel((width-width_mod, height-height_mod))
 
-        # Converts the RGB values to binary
+        # Converts the RGB values to their binary representation
         binary_red = list(bin(r)[2:].zfill(8))
         binary_green = list(bin(g)[2:].zfill(8))
         binary_blue = list(bin(b)[2:].zfill(8))
 
-        # Fills the pixel with a new RGB value
+        # Modifies the least significant bit of the RGB value
         if length_counter > 0:
-            # Take the least significant bit and modify it according
+            # Modify the RGB values to insert message length
             binary_red[7] = binaryMsgLength[length_counter]
             length_counter -= 1
             binary_red = int("".join(binary_red), 2)
@@ -79,16 +89,16 @@ def encode_message(im, file_name, file_extension, user_message, output_file):
             im.putpixel((width - width_mod, height - height_mod),
                         (binary_red, binary_green, binary_blue))
         elif times_to_loop > 0:
-            # Modify the RGB to insert our message
-            if letter_counter < msgLength * 8:
+            # Modify the RGB values to insert our message
+            if letter_counter < message_length * 8:
                 binary_red[7] = binaryMessage[letter_counter]
                 letter_counter += 1
 
-            if letter_counter < msgLength * 8:
+            if letter_counter < message_length * 8:
                 binary_green[7] = binaryMessage[letter_counter]
                 letter_counter += 1
 
-            if letter_counter < msgLength * 8:
+            if letter_counter < message_length * 8:
                 binary_blue[7] = binaryMessage[letter_counter]
                 letter_counter += 1
 
@@ -108,18 +118,19 @@ def encode_message(im, file_name, file_extension, user_message, output_file):
             height_mod += 1
             width_mod = 1
 
+    # Save our encoded file with the user specified name and png format
     im.save("{}.png".format(output_file))
 
 
-#######################################################################
-# This function extracts data from an image. It grabs a pixel at a    #
-# certain xy coordinate, starting at the bottom right, determines the #
-# length and content of the message by checking the least significant #
-# bit of each RGB value and translates it into a readable string.     #
-#                                                                     #
-# Function input: Image object                                        #
-# Function returns: Decoded message                                   #
-#######################################################################
+###############################################################################
+# This function extracts data from an image. It grabs a pixel at a certain xy #
+# coordinate, starting at the bottom right, determines the length and content #
+# of the message by checking the least significant bit of each RGB value and  #
+# translates it into a readable string.                                       #
+#                                                                             #
+# Function input: Image object                                                #
+# Function returns: Decoded message                                           #
+###############################################################################
 def decode_message(im):
     # Determines the width and height of the image
     width, height = im.size
@@ -136,7 +147,7 @@ def decode_message(im):
 
     # This while loop handles the length of the message
     while times_to_loop > 0:
-        # Gets the RGB value at a given xy coordinate
+        # Gets the RGB value at a given XY coordinate
         r, g, b = im.getpixel((width - width_mod, height - height_mod))
 
         # Converts the RGB values to binary
@@ -208,19 +219,21 @@ def decode_message(im):
             height_mod += 1
             width_mod = 1
 
-    # Bits are grouped joined into groups of 8, then turned into a
-    # character based off of their ascii value.
+    # Decoded bits are joined into groups of 8, then turned into a character
+    # based off of their ascii value.
     decoded_message = []
     i = 0
     while i != num_of_bits_in_msg:
         decoded_message.append(str(chr(int("".join(bits_message[i:i+8]), 2))))
         i += 8
 
+    # Joins our list so it can be displayed as a string
     decoded_message = "".join(decoded_message)
     return decoded_message
 
 
 def main():
+    # Creates a parser to allow us to handle command line arguments
     parser = argparse.ArgumentParser(description="Encode or decode an image")
     parser.add_argument("-d", "--decode", help="file to decode")
     parser.add_argument("-i", "--inFile", help="path to file to encode")
@@ -231,10 +244,10 @@ def main():
     # If/else statements to determine what the user would like to do.
     # The first one prevents choosing encoding and decoding at the
     # same time. The second uses an input file, a message, and
-    # the name of an ouptut file to execute. The third just needs
+    # the name of an output file to execute. The third just needs
     # a file to decode. Otherwise, our user has messed up and we exit.
     if args.encode is not None and args.decode is not None:
-        print("You need to choose between encoding and decoding bro")
+        print("You need to choose between encoding and decoding!")
         sys.exit()
     elif args.encode is not None and \
         args.inFile is not None and \
@@ -245,14 +258,11 @@ def main():
         message = args.encode
         output = args.outFile
 
-        # Splits the file name from the extension so we can change it to png
-        file_name, file_extension = os.path.splitext(file)
-
-        # Creates an image object to allow us to interact with the user's image
+        # Creates an image object to allow interaction with the user's image
         im = Image.open(file)
 
         # Encodes the message and saves it
-        encode_message(im, file_name, file_extension, message, output)
+        encode_message(im, message, output)
     elif args.decode is not None and \
         args.encode is None and \
             args.inFile is None and \
@@ -260,13 +270,13 @@ def main():
         # Sets the file variable to the image the user has entered
         file = args.decode
 
-        # Creates an image object to allow us to interact with the user's image
+        # Creates an image object to allow interaction with the user's image
         im = Image.open(file)
 
         # Decodes the message and reports it to the user
         print(decode_message(im))
     else:
-        print("We're done here...")
+        print("You messed up somewhere... Try again.")
         sys.exit()
 
 if __name__ == "__main__":
